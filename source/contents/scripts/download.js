5552c8de105048df0670bc609c26e8deb0240518
Gordon Hall <gordon@gordonwritescode.com>
2014-03-16 11:16:17 -0400

    added script for platform download link - changed to red design, added tv shows feature block

---
+++ source/contents/scripts/download.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  var downloads = {
+    linux: "https://github.com/untv/untv/releases/download/v0.6.0/untv-0.6.0-linux64.zip",
+    win: "https://github.com/untv/untv/releases/download/v0.6.0/untv-0.6.0-win.zip",
+    osx: "https://github.com/untv/untv/releases/download/v0.6.0/untv-0.6.0-osx.zip"
+  };
+
+  function getOperatingSystem() {
+    var os = null;
+    if (navigator.appVersion.indexOf("Win")!=-1) os="win";
+    if (navigator.appVersion.indexOf("Mac")!=-1) os="osx";
+    if (navigator.appVersion.indexOf("Linux")!=-1) os="linux";
+    return os;
+  };
+
+  var download = $("#download a");
+  var system   = getOperatingSystem();
+
+  if (system) {
+    download.attr("href", downloads[system]);
+  }
+});
